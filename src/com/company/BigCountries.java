3ac13fc0f406bc46026451d06e7e719afa62a236
Wangmmx <437299476@qq.com>
2018-07-07 22:14:31 +0800

    fix(exercise): add day3

---
+++ src/com/company/BigCountries.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BigCountries {
+
+    //Big Countries No.595
+    public static void main(String[] args) {
+        //sql statement
+        String sql = "select name, population, area from World where area >3000000 or population>25000000";
+    }
+
+
+}
