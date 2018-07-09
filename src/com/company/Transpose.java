7c74ea7f814b609a59ddf59998b168ebb1c945bc
Wangmmx <437299476@qq.com>
2018-07-09 18:22:44 +0800

    fix(exercise): add in advance

---
+++ src/com/company/Transpose.java
@@ -0,0 +1,29 @@
+
+package com.company;
+
+import java.util.Arrays;
+
+public class Transpose {
+
+    //Transpose Matrix  No.868
+    public static void main(String[] args) {
+//        int[][] A;
+        int[][] A = {{1,2,3,4},{4,5,6,7}};
+        int m = A.length;
+        int n = A[0].length;
+        int[][] B = new int[n][m];
+        int temp;
+        for (int i =0; i< m; i++) {
+            for (int j =0; j< n; j++) {
+                temp = A[i][j];
+                B[j][i] = temp;
+            }
+        }
+        for (int k =0; k< B.length; k++) {
+            System.out.println(Arrays.toString(B[k]));
+        }
+        System.out.println(B);
+    }
+
+
+}
