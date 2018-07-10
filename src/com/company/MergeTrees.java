d5e2b61d1bbf16005381598d906646d7eee359c2
Wangmmx <437299476@qq.com>
2018-07-10 23:42:41 +0800

    fix(exercise): add in order to compile

--- src/com/company/MergeTrees.java
+++ src/com/company/MergeTrees.java
@@ -1,6 +1,7 @@
 
 package com.company;
 
+import javax.swing.tree.TreeNode;
 import java.util.Arrays;
 /**
  * Definition for a binary tree node.
@@ -14,12 +15,19 @@ import java.util.Arrays;
 public class MergeTrees {
 
     //Transpose Matrix  No.868
-    public static void main(String[] args) {
-//
-
-        System.out.println();
-        //
+ 
+ public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        else if (t1 == null) return t2;
+        else if (t2 == null) return t1;
+     //   TreeNode n= new TreeNode(t1.val + t2.val);
+     //   n.left = mergeTrees(t1.left, t2.left);
+     //   n.right = mergeTrees(t1.right, t2.right);
+      //  return n;
+     return t1;
     }
+
+
 
 
 }
