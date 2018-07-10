b42aeb2cad875230e33cdbc069e95c1fee780ed4
Wangmmx <437299476@qq.com>
2018-07-10 23:48:02 +0800

    fix(tree): add class TreeNode

--- src/com/company/MergeTrees.java
+++ src/com/company/MergeTrees.java
@@ -1,7 +1,7 @@
 
 package com.company;
 
-import javax.swing.tree.TreeNode;
+
 import java.util.Arrays;
 /**
  * Definition for a binary tree node.
@@ -12,6 +12,14 @@ import java.util.Arrays;
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
 public class MergeTrees {
 
     //Transpose Matrix  No.868
@@ -20,11 +28,10 @@ public class MergeTrees {
         if (t1 == null && t2 == null) return null;
         else if (t1 == null) return t2;
         else if (t2 == null) return t1;
-     //   TreeNode n= new TreeNode(t1.val + t2.val);
-     //   n.left = mergeTrees(t1.left, t2.left);
-     //   n.right = mergeTrees(t1.right, t2.right);
-      //  return n;
-     return t1;
+        TreeNode n= new TreeNode(t1.val + t2.val);
+        n.left = mergeTrees(t1.left, t2.left);
+        n.right = mergeTrees(t1.right, t2.right);
+        return n;
     }
 
 
