1ab3788505a1f99ecbcfb7786be93220903597b4
Wangmmx <437299476@qq.com>
2018-07-13 00:12:41 +0800

    fix(exercise): add toLowerCase

---
+++ src/com/company/ToLowerCase.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ToLowerCase {
+
+    //Judge Route Circle No.657
+    public static void main(String[] args) {
+//     String str
+       String str = "AHIHDEO";
+       System.out.println(str.toLowerCase());
+    }
+}
