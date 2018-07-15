1dd340b9b7e0dd052256603d623acc2db99bc9ab
Wangmmx <437299476@qq.com>
2018-07-15 21:05:54 +0800

    fix(exercise): add encode & decode

---
+++ src/com/company/Codec.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Codec {
+
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String url = longUrl + "@wangmeng";
+        return url;
+
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        String url = shortUrl.replace("@wangmeng", "");
+        return url;
+    }
+
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+
+}
+
