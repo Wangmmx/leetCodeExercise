4cd5512839e6953e6330b0f25dbffcadb4fdaac2
Wangmmx <437299476@qq.com>
2018-07-15 14:28:51 +0800

    fix(exercise): fix skyline problem

--- src/com/company/MaxIncreaseKeepingSkyline.java
+++ src/com/company/MaxIncreaseKeepingSkyline.java
@@ -9,7 +9,6 @@ public class MaxIncreaseKeepingSkyline {
     public static void main(String[] args) {
 //     int[][] grid
        int[][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
-       int num;
        List topToBottom = new ArrayList();
        List leftToRight = new ArrayList();
        int[][] grid2 = new int[grid[0].length][grid.length];
@@ -28,17 +27,30 @@ public class MaxIncreaseKeepingSkyline {
             leftToRight.add(max);
             topToBottom.add(max2);
         }
-        System.out.println(leftToRight);
-        System.out.println(topToBottom);
+        //System.out.println(leftToRight);
+        //System.out.println(topToBottom);
         int[][] newGrid = new int[grid.length][grid[0].length];
         for (int k=0;k<grid.length;k++) {
             for (int m =0;m<grid[0].length;m++) {
-                newGrid[k][m] = Integer.valueOf(leftToRight.get(k)>topToBottom.get(m)?leftToRight.get(k):topToBottom.get(m);
+                int aa = Integer.parseInt(leftToRight.get(k).toString());
+                int bb = Integer.parseInt(topToBottom.get(m).toString());
+                int minValue = Math.min(aa,bb);
+                newGrid[k][m] = minValue;
+               // System.out.println(minValue);
             }
         }
-
-
-
-      // System.out.println();
+        int oldSum = 0;
+        for (int i =0; i<grid.length;i++) {
+            for (int j =0; j<grid[0].length;j++) {
+                oldSum += grid[i][j];
+            }
+        }
+        int newSum = 0;
+        for (int i =0; i<newGrid.length;i++) {
+            for (int j =0; j<newGrid[0].length;j++) {
+                newSum += newGrid[i][j];
+            }
+        }
+        System.out.println(newSum-oldSum);
     }
 }
