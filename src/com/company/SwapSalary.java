9442379f85322a5baa7f8557d4f79e7b3480b581
Wangmmx <437299476@qq.com>
2018-07-18 18:14:33 +0800

    fix(exercise): add sql swap salary

---
+++ src/com/company/SwapSalary.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class SwapSalary {
+
+    //Swap Salary No.627
+    public static void main(String[] args) {
+        //sql statement
+        String sql = "update salary set sex = (case sex when \"f\" then \"m\" else \"f\" end)";
+        //输出性别相反的数据
+    }
+
+
+}
