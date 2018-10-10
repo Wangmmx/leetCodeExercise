7e8b225d44a3a4d1327324564bb91e79a7721138
Wangmmx <437299476@qq.com>
2018-10-10 14:42:30 +0800

    fix(new exercise): submit before working

---
+++ src/com/company/SortArrayByPartity.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+
+/**
+ * Created by lenovo on 2018/10/10.
+ */
+public class SortArrayByPartity {
+    public static void main(String[] args) {
+        int[] A = {3,1,2,4};
+        int[] result = new int[A.length];
+
+        System.out.println(result.toString());
+
+        }
+
+}
+
