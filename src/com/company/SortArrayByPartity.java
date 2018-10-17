557580d9399aff4eb863320ebf545c4eabb08c44
Wangmmx <437299476@qq.com>
2018-10-18 00:26:23 +0800

    Revert "feat(exercise): add a sort method"
    This reverts commit a3af1b67472399bc12d7e8a77d039bc53ed3c04b.

--- src/com/company/SortArrayByPartity.java
+++ src/com/company/SortArrayByPartity.java
@@ -1,29 +1,18 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by lenovo on 2018/10/10.
  */
 public class SortArrayByPartity {
     public static void main(String[] args) {
-//        public int[] sortArrayByParity(int[] A) {
-        int[] A = {3, 1, 2, 4};
+        int[] A = {3,1,2,4};
         int[] result = new int[A.length];
-        int m = 0;
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] % 2 == 0)
-                result[m++] = A[i];
 
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] % 2 == 1)
-                result[m++] = A[i];
-        System.out.println(Arrays.toString(result));
-        //return result;
-    }
+        System.out.println(result.toString());
 
+        }
 
 }
-
 
