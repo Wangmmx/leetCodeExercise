a3af1b67472399bc12d7e8a77d039bc53ed3c04b
王梦 <wangmeng@wangmengdeMacBook-Pro.local>
2018-10-18 00:12:13 +0800

    feat(exercise): add a sort method

--- src/com/company/SortArrayByPartity.java
+++ src/com/company/SortArrayByPartity.java
@@ -1,18 +1,29 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by lenovo on 2018/10/10.
  */
 public class SortArrayByPartity {
     public static void main(String[] args) {
-        int[] A = {3,1,2,4};
+//        public int[] sortArrayByParity(int[] A) {
+        int[] A = {3, 1, 2, 4};
         int[] result = new int[A.length];
+        int m = 0;
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 0)
+                result[m++] = A[i];
 
-        System.out.println(result.toString());
+        for (int i = 0; i < A.length; ++i)
+            if (A[i] % 2 == 1)
+                result[m++] = A[i];
+        System.out.println(Arrays.toString(result));
+        //return result;
+    }
 
-        }
 
 }
+
 
