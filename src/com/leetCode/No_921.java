4761a9f29ae825c17efedf91078c306a40169ee5
Wangmmx <437299476@qq.com>
2018-11-15 15:08:51 +0800

    feat(code): add 921

--- src/com/leetCode/No_921.java
+++ src/com/leetCode/No_921.java
@@ -6,15 +6,23 @@ import java.util.Stack;
 //921. Minimum Add to Make Parentheses Valid
 public class No_921 {
     public static void main(String[] args) {
-        System.out.println((new Solution().minAddToMakeValid("()))((")));
+        System.out.println((new Solution().minAddToMakeValid("))))(()")));
     }
 
     static class Solution {
         public int minAddToMakeValid(String S) {
-
-
-            return 1;
-
+            int count = 0;
+            int left = 0;
+            char[] c = S.toCharArray();
+            for (int i = 0; i < c.length; i++) {
+                if (c[i] == '(') left++;
+                else {
+                    if ( left!=0 ) {
+                        left--;
+                    } else count++;
+                }
+            }
+            return count + left;
         }
     }
 }
